4c9260fb212b93a13c757a88ffd3bf4aefc44ef1
unknown <Valarie.Regas@gmail.com>
2017-09-18 09:05:47 -0400

    basic Giphy query working

--- giftastic.js
+++ giftastic.js
@@ -1,76 +1,93 @@
 //Starting array
 
 var topics = [
-	"Judo",
-	"BJJ",
-	"muay thai",
-	"freestyle wrestling",
-	"jiu-jitsu",
-	"boxing"
-]
+    "Judo",
+    "BJJ",
+    "muay thai",
+    "freestyle wrestling",
+    "jiu-jitsu",
+    "boxing"
+];
 
-$(document).ready(function(){
-//Functions
+$(document).ready(function() {
+            //Functions
 
-// Function to create buttons from array
-function displayButtons() { 
+            // Function to create buttons from array
+            function displayButtons() {
 
-	for (var i = 0; i < topics.length; i++) {
+                for (var i = 0; i < topics.length; i++) {
 
-		var newButton = $("<button>");
-		newButton.addClass("btn btn-default");
-		newButton.attr("type", "button");
-		newButton.append(topics[i]);
-		newButton.attr("value", topics[i]);
-		$("#newButtonDiv").append(newButton);
+                    var newButton = $("<button>");
+                    newButton.addClass("btn btn-default");
+                    newButton.attr("type", "button");
+                    newButton.append(topics[i]);
+                    newButton.attr("value", topics[i]);
+                    $("#newButtonDiv").append(newButton);
 
-		console.log(topics);
+                    console.log(topics);
 
-	}
-}
-debugger;
-displayButtons();
-
-
-// // The ajax to get the API info
-
-$(".btn-default").on('click', function() {
-
-	function getGifs() {
-
-var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=1e1f311560de4786917912fb4afee7e5&limit=10");
-
-$.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
-
-        	console.log(response);
-         })
-	}
-}); 
-
-// Function to create images in the div
-
-function addImages(response) {
-
-	for (var j = 0; j < response.data.length; j++) {
-
-		var newGif = $("<img>");
-        newGif.addClass("newGifs");
-        newGif.attr("src", "response[j].data.images.fixed_width_still.url");
-        $("#imageHolder").append(newGif);
-    		
-    	}
-}
+                }
+            }
+            debugger;
+            displayButtons();
 
 
+            // // The ajax to get the API info
+
+            
+                    function getGifs(topic) {
+
+                        var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=1e1f311560de4786917912fb4afee7e5&limit=10");
+
+                        console.log(queryURL);
+
+                        $.ajax({
+                            url: queryURL,
+                            method: "GET"
+                        }).done(function(response) {
+
+                                console.log(response.data);
+                                addImages(response);
+
+                       	 });
+                        }
+
+                                // Function to create images in the div
+
+                                function addImages(response) {
+
+                                	$("#imageHolder").empty();
+
+                                    for (var j = 0; j < response.data.length; j++) {
+
+                                        var newGif = $("<img>");
+                                        newGif.addClass("newGifs");
+                                        newGif.attr("src", response.data[j].images.fixed_width_still.url);
+                                        newGif.attr('class', "img-thumbnail");
+                                        $("#imageHolder").append(newGif);
+
+                                    // Function to animate the retrieved still images
+                                		var static = response.data[j].images.fixed_width_still.url
+                                		var animate = response.data[j].images.fixed_width.url
 
 
+		                                if (newGif === animate) {
+		                                    $(this).attr('src', $(this).attr('static'));
+		                                } else {
+		                                    $(this).attr('src', $(this).attr('animate'));
+
+		                                }
 
 
+                                    }
+                                }              
 
+                
 
+        $(".btn-default").on('click', function() {
 
-//closing of doc ready
-});
+            	console.log("click");
+            	getGifs($(this).attr('value'));
+        });
+                //closing of doc ready
+            });
