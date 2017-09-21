e8ce0b696199c0190fba3dbcac1ba5d1ef319427
unknown <Valarie.Regas@gmail.com>
2017-09-21 16:49:52 -0400

    minor bug fix

--- giftastic.js
+++ giftastic.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
 
     function getGifs(topic) {
 
-        var queryURL = ("http://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=1e1f311560de4786917912fb4afee7e5&limit=10");
+        var queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=1e1f311560de4786917912fb4afee7e5&limit=10");
 
         console.log(queryURL);
 
