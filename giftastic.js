028de1eb58c2a8409386281e651a505af4cf4a0b
unknown <Valarie.Regas@gmail.com>
2017-09-22 18:40:26 -0400

    changed click function

--- giftastic.js
+++ giftastic.js
@@ -110,7 +110,7 @@ $(document).ready(function() {
 
 
 
-    $(".topicBtn").on('click', function() {
+    $(document).on('click',".topicBtn" function() {
 
         console.log("click");
         getGifs($(this).attr('value'));
